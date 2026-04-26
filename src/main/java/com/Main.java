91a4229cbeb9199eec12b83877d5a39e36d1c90f
hiysk0610-hi <hiysk0610@gmail.com>
2026-04-26 18:38:02 +0900

    3단계

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -23,7 +23,7 @@ public class Main {
                 String wiseSayingContent = scanner.nextLine().trim();
                 System.out.println("작가: ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
-
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
         scanner.close();
