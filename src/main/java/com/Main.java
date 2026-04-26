4bfcafc9c04cb67765518aa1d23d33e6a9a67304
hiysk0610-hi <hiysk0610@gmail.com>
2026-04-26 18:32:17 +0900

    2단계

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -18,8 +18,16 @@ public class Main {
             if (cmd.equals("종료")) {
                 break;
             }
+            else if (cmd.equals("등록")) {
+                System.out.println("명언: ");
+                String wiseSayingContent = scanner.nextLine().trim();
+                System.out.println("작가: ");
+                String wiseSayingAuthor = scanner.nextLine().trim();
+
+            }
         }
         scanner.close();
     }
-}
+    }
+
 
